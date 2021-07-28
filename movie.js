2003cd4117383f53895610437d1e0e406678fedb
anurag-k01 <86410014+anurag-k01@users.noreply.github.com>
2021-07-28 21:15:34 +0530

    Add files via upload

--- movie.js
+++ movie.js
@@ -2,7 +2,10 @@ async function getMovie() {
   let city = document.getElementById("city").value;
   let show = document.getElementById("show");
   let div = document.createElement("div");
-
+  let error_div = document.getElementById("ereror");
+  let recommend = document.createElement("p");
+  recommend.innerHTML = "Recommended";
+  recommend.style.color = "red";
   div.setAttribute("class", "movie_div");
   try {
     let res = await fetch(`http://www.omdbapi.com/?t=${city}&apikey=e2dc73e1`);
@@ -14,17 +17,28 @@ async function getMovie() {
     year.innerHTML = `Year :-  ${data.Year}`;
     let rating = document.createElement("p");
     rating.innerHTML = `Rating :-  ${data.imdbRating}`;
+
     let poster = document.createElement("img");
     poster.setAttribute("class", "poster");
     poster.src = data.Poster;
-    div.append(poster, title, year, rating);
+    if (data.Response == "False") {
+      let img = document.createElement("img");
+      img.src =
+        "https://th.bing.com/th/id/OIP.C_HN1Ny75PfkPGmK8YtR1gHaDT?w=345&h=156&c=7&o=5&dpr=1.25&pid=1.7";
 
-    show.append(div);
-
-    console.log(data);
+      error_div.append(img);
+    } else {
+      if (data.imdbRating > 8.5) {
+        error_div.innerHTML = null;
+        div.append(poster, title, year, rating, recommend);
+      } else {
+        error_div.innerHTML = null;
+        div.append(poster, title, year, rating);
+      }
+      show.append(div);
+    }
+    console.log("data", data);
   } catch (error) {
-    div.append(error);
-
-    show.append(div);
+    console.log(error);
   }
 }
